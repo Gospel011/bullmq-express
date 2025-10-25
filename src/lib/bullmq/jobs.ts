0ac9834fc67ddba402972fca2b4da46759ee6213
Gospel <gospel@Gospels-MacBook-Pro.local>
2025-10-25 23:39:43 +0100

    restructured queues to have one queue per file

--- src/lib/bullmq/jobs.ts
+++ src/lib/bullmq/jobs.ts
@@ -1,23 +1,5 @@
-import { Queue } from "bullmq";
-
-const greetQueue = new Queue<GreetData>("greet", {
-  connection: {
-    host: "127.0.0.1",
-    port: 6379,
-  },
-  defaultJobOptions: {
-    removeOnComplete: {
-      age: 60,
-    //   count: 1000,
-    },
-    removeOnFail: {
-      age: 1 * 24 * 60,
-      count: 10000,
-    },
-  },
-});
+import greetQueue from "./queues/greet_queue.js";
 
 
-await greetQueue.add("greet:aaron", { name: "Aaron" });
 
-export default greetQueue;
+// await greetQueue.add("greet:aaron", { name: "Aaron" });
---
+++ src/lib/bullmq/queues/greet_queue.ts
@@ -0,0 +1,20 @@
+import { Queue } from "bullmq";
+
+const greetQueue = new Queue<GreetData>("greet", {
+  connection: {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+  defaultJobOptions: {
+    removeOnComplete: {
+      age: 60,
+      //   count: 1000,
+    },
+    removeOnFail: {
+      age: 1 * 24 * 60,
+      count: 10000,
+    },
+  },
+});
+
+export default greetQueue
